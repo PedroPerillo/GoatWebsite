ce367ade6e6be49dffcc482a4b244dd9bde76692
JeffDing27 <rud300@ensign.edu>
2024-03-04 15:31:29 -0700

    kfdhjwd

--- gymbro/my-project/src/Home.js
+++ gymbro/my-project/src/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
             <div className="App">
 
 
-                <header className="bg-white/40 backdrop-blur-sm sticky absolute z-40 shadow-lg top-0">
+                <header className="bg-white/40 backdrop-blur-sm  absolute z-40 shadow-lg top-0">
                     <nav className="  mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
                          aria-label="Global">
                         <div className="flex lg:flex-1">
