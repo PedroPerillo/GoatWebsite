ac1a5b8e9bbe67e5ba1668b0c79cc142d652cc6c
JeffDing27 <rud300@ensign.edu>
2024-03-06 14:47:08 -0700

    set up stream

--- gymbro/my-project/src/Home.js
+++ gymbro/my-project/src/Home.js
@@ -442,7 +442,7 @@ export default function Home() {
                 </div>
 
 
-                <footer className="bg-gray-800" aria-labelledby="footer-heading">
+                <footer className="bg-gray-700" aria-labelledby="footer-heading">
                     <h2 id="footer-heading" className="sr-only">Footer</h2>
                     <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
                         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
