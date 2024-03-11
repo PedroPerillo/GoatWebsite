6df2021e348fded30dc721b97ab726a8e241a594
Samuel Raja <sar785@ensign.edu>
2024-03-11 14:42:32 -0600

    Update Home

--- gymbro/my-project/src/Home.js
+++ gymbro/my-project/src/Home.js
@@ -94,16 +94,13 @@ export default function Home() {
                 <div className="isolate overflow-hidden bg-slate-300">
                     <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
                         <div className="mx-auto max-w-4xl">
-                            <h2 className="text-base font-semibold leading-7 text-indigo-400">Pricing</h2>
+                            <h2 className="text-xl font-bold leading-7 text-indigo-400">Pricing</h2>
                             <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">The right price
                                 for you, <br
                                     className="hidden sm:inline lg:hidden"/>whoever you are</p>
                         </div>
                         <div className="relative mt-6">
-                            <p className="mx-auto max-w-2xl text-lg leading-8 text-white/60">Lorem ipsum dolor sit, amet
-                                consectetur
-                                adipisicing elit. Velit numquam eligendi quos odit doloribus molestiae voluptatum.</p>
-                            <svg viewBox="0 0 1208 1024"
+                            <p className="mx-auto max-w-2xl text-lg leading-8 text-white/60">"Unlock limitless possibilities with our tailored pricing plans. Elevate your experience, invest in success today!"</p>                            <svg viewBox="0 0 1208 1024"
                                  className="absolute -top-10 left-1/2 -z-10 h-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:-top-12 md:-top-20 lg:-top-12 xl:top-0">
                                 <ellipse cx="604" cy="512" fill="url(#6d1bd035-0dd1-437e-93fa-59d316231eb0)" rx="604"
                                          ry="512"/>
@@ -124,26 +121,37 @@ export default function Home() {
                                         className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
                                         <div>
                                             <h3 id="tier-hobby"
-                                                className="text-base font-semibold leading-7 text-indigo-600">Hobby</h3>
+                                                className="text-base font-semibold leading-7 text-indigo-600">Basic</h3>
                                             <div className="mt-4 flex items-baseline gap-x-2">
                                                 <span
-                                                    className="text-5xl font-bold tracking-tight text-gray-900">$49</span>
+                                                    className="text-5xl font-bold tracking-tight text-gray-900">$14.99</span>
                                                 <span
                                                     className="text-base font-semibold leading-7 text-gray-600">/month</span>
                                             </div>
-                                            <p className="mt-6 text-base leading-7 text-gray-600">Modi dolorem expedita
-                                                deleniti. Corporis
-                                                iste qui inventore pariatur adipisci vitae.</p>
-                                            <ul role="list" className="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                                            <p className="mt-6 text-base leading-7 text-gray-600">Enjoy access to state-of-the-art equipment, expert guidance from certified trainers, and a supportive community to help you reach your fitness goals.</p>
+                                            <ul role="list" className="mt-10 space-y-4 text-sm leading-6 text-gray-600"> 
+                                                <li className="flex gap-x-3">
+                                                    <svg className="h-6 w-5 flex-none text-indigo-600" 
+                                                         viewBox="0 0 20 20" fill="currentColor"
+                                                         aria-hidden="true">
+                                                        <path fill-rule="evenodd"
+                                                              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                                                              clip-rule="evenodd"/>
+                                                              
+                                                    </svg>
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Gym Membership</span>
+</li>
+
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
+                                                    
                                                          viewBox="0 0 20 20" fill="currentColor"
                                                          aria-hidden="true">
                                                         <path fill-rule="evenodd"
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    5 products
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Training sessions</span>
                                                 </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
@@ -153,7 +161,7 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    Up to 1,000 subscribers
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Basic fitness classes</span>
                                                 </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
@@ -163,39 +171,33 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    Basic analytics
-                                                </li>
-                                                <li className="flex gap-x-3">
-                                                    <svg className="h-6 w-5 flex-none text-indigo-600"
-                                                         viewBox="0 0 20 20" fill="currentColor"
-                                                         aria-hidden="true">
-                                                        <path fill-rule="evenodd"
-                                                              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                                              clip-rule="evenodd"/>
-                                                    </svg>
-                                                    48-hour support response time
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Nutrition Supplements</span>
                                                 </li>
                                             </ul>
                                         </div>
                                         <a href="#" aria-describedby="tier-hobby"
-                                           className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                                            started today</a>
+                                           className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's Get in Shape</a>
                                     </div>
                                     <div
                                         className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10">
                                         <div>
                                             <h3 id="tier-team"
-                                                className="text-base font-semibold leading-7 text-indigo-600">Team</h3>
+                                                className="text-base font-semibold leading-7 text-indigo-600">Premium</h3>
                                             <div className="mt-4 flex items-baseline gap-x-2">
                                                 <span
-                                                    className="text-5xl font-bold tracking-tight text-gray-900">$79</span>
+                                                    className="text-5xl font-bold tracking-tight text-gray-900">$29.99</span>
                                                 <span
                                                     className="text-base font-semibold leading-7 text-gray-600">/month</span>
                                             </div>
-                                            <p className="mt-6 text-base leading-7 text-gray-600">Explicabo quo fugit
-                                                vel facere ullam
-                                                corrupti non dolores. Expedita eius sit sequi.</p>
+                                            <p className="mt-6 text-base leading-7 text-gray-600">Unlock exclusive perks and advanced features for ultimate fitness success.</p>
                                             <ul role="list" className="mt-10 space-y-4 text-sm leading-6 text-gray-600">
+                                            
+        <li className="flex gap-x-3">
+            <svg className="h-6 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+            </svg>
+            <Link to="/premium-membership" className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Premium Membership</Link>
+        </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
                                                          viewBox="0 0 20 20" fill="currentColor"
@@ -204,7 +206,7 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    Unlimited products
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Elite Fitness Plan</span>
                                                 </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
@@ -214,7 +216,7 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    Unlimited subscribers
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Enhanced Nutrition Supplements</span>
                                                 </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
@@ -224,7 +226,7 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    Advanced analytics
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Exclusive Branded Apparel</span>
                                                 </li>
                                                 <li className="flex gap-x-3">
                                                     <svg className="h-6 w-5 flex-none text-indigo-600"
@@ -234,32 +236,20 @@ export default function Home() {
                                                               d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
                                                               clip-rule="evenodd"/>
                                                     </svg>
-                                                    1-hour, dedicated support response time
-                                                </li>
-                                                <li className="flex gap-x-3">
-                                                    <svg className="h-6 w-5 flex-none text-indigo-600"
-                                                         viewBox="0 0 20 20" fill="currentColor"
-                                                         aria-hidden="true">
-                                                        <path fill-rule="evenodd"
-                                                              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                                              clip-rule="evenodd"/>
-                                                    </svg>
-                                                    Marketing automations
+                                                    <span className="cursor-pointer transform transition-transform duration-900 hover:text-red-700 hover:scale-110">Annual Membership Club</span>
                                                 </li>
                                             </ul>
                                         </div>
                                         <a href="#" aria-describedby="tier-team"
-                                           className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                                            started today</a>
+                                           className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's get Ripped</a>
                                     </div>
 
                                     <div
                                         className="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
                                         <div className="lg:min-w-0 lg:flex-1">
                                             <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Discounted</h3>
-                                            <p className="mt-1 text-base leading-7 text-gray-600">Dolor dolores
-                                                repudiandae doloribus. Rerum
-                                                sunt aut eum. Odit omnis non voluptatem sunt eos nostrum.</p>
+                                            <p className="mt-1 text-base leading-7 text-gray-600">"Exciting update - Unlock exclusive savings on gym app memberships: Get fit for less with our 
+                                            limited-time discounts!"</p>
                                         </div>
                                         <a href="#"
                                            className="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Buy
@@ -268,7 +258,7 @@ export default function Home() {
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>  
                 </div>
 
 
