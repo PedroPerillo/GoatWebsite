09e01dcc0eca75166d9132a95e7d1dcc191ce44e
Juan Palomeque <jup017@ensign.edu>
2024-03-13 13:59:01 -0600

    update

--- gymbro/my-project/src/pages/Home.jsx
+++ gymbro/my-project/src/pages/Home.jsx
@@ -304,8 +304,7 @@ export default function Home() {
                 G.O.A.T.
               </h2>
               <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-                Incididunt sint fugiat pariatur cupidatat consectetur sit cillum
-                anim id veniam aliqua proident excepteur commodo do ea.
+                Sculpt your future: Transform with GOAT!
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
@@ -348,13 +347,12 @@ export default function Home() {
           <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
+                Fit Check
                 <br />
-                Start using our app today.
+                Empower Your Fitness Journey!
               </h2>
               <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-                Incididunt sint fugiat pariatur cupidatat consectetur sit cillum
-                anim id veniam aliqua proident excepteur commodo do ea.
+                Unleash Your Inner Strength
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
