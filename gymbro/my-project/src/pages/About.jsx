5491e460a9cefa153f8e6c440519e412a23c9f08
Pedro Perillo <pedrofperillo@gmail.com>
2024-03-29 22:02:44 -0600

    addes changes

--- gymbro/my-project/src/pages/About.jsx
+++ gymbro/my-project/src/pages/About.jsx
@@ -3,7 +3,11 @@ import PageLayout from "../components/PageLayout";
 export default function About () {
     return(
         <PageLayout>
-
+            <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-48 shadow-2xl">
+                <div className="text-4xl font-bold text-white font-serif">
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
             <h1>About</h1>
 
         </PageLayout>
