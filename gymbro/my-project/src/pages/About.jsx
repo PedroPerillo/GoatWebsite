3c714fc7075f28415cd391d19dcfac2476f29fb6
Pedro Perillo <pedrofperillo@gmail.com>
2024-03-29 21:14:00 -0600

    changes made

--- gymbro/my-project/src/pages/About.jsx
+++ gymbro/my-project/src/pages/About.jsx
@@ -3,9 +3,66 @@ import PageLayout from "../components/PageLayout";
 export default function About () {
     return(
         <PageLayout>
+            <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-48 shadow-2xl">
+                <div className="text-4xl font-bold text-white font-serif">
+                    <h1>Our Product</h1>
+                </div>
+            </div>
+            <div className="text-center">
+                <div className="flex flex-row">
+                    <h1 className="text-2xl font-bold p-16 font-serif basis-1/3 text-nowrap">
+                        Let's Start a Conversation!
+                    </h1>
+                    <h1 className="text-2xl font-bold p-16 font-serif basis-2/3 text-nowrap">
+                        Message
+                    </h1>
 
-            <h1>About</h1>
-
+                    <input
+                        className="mt-20 mx-20  bg-gray-200 appearance-none border-3 border-purple-900 rounded w-full px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        placeholder="Your message here..."
+                    />
+                </div>
+            </div>
+            <form class="w-full max-w-sm">
+                <div class="flex flex-row mb-6">
+                    <div class="basis-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Full Name
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="basis-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Email
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3"></div>
+                    <label class="md:w-2/3 block text-gray-500 font-bold">
+                        <input class="mr-2 leading-tight" type="checkbox" />
+                        <span class="text-sm">Send me your newsletter!</span>
+                    </label>
+                </div>
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <button
+                            class="mb-8 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="button"
+                        >
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </form>
         </PageLayout>
     )
 }
--- gymbro/my-project/src/pages/FitCheck.jsx
+++ gymbro/my-project/src/pages/FitCheck.jsx
@@ -5,23 +5,66 @@ import {Link} from "react-router-dom";
 export default function FitCheck () {
     return(
         <PageLayout>
-            <div className='bg-fuchsia-200 sticky top-0 p-10 space-y-10'>
-                <div className='bg-violet-600/40 p-5 rounded-md shadow-2xl sticky top-0 text-left'>
+            <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-48 shadow-2xl">
+                <div className="text-4xl font-bold text-white font-serif">
                     <h1>FitCheck</h1>
+                </div>
+            </div>
+            <div className="text-center">
+                <div className="flex flex-row">
+                    <h1 className="text-2xl font-bold p-16 font-serif basis-1/3 text-nowrap">
+                        Let's Start a Conversation!
+                    </h1>
+                    <h1 className="text-2xl font-bold p-16 font-serif basis-2/3 text-nowrap">
+                        Message
+                    </h1>
 
-                </div>
-                <div className='bg-violet-600/70 p-5 rounded-md shadow-2xl sticky top-0'>
-                </div>
-                <div className='bg-fuchsia-200/40 p-5 rounded-md shadow-2xl sticky top-0'>
-                    <h1>FitCheck</h1>
+                    <input
+                        className="mt-20 mx-20  bg-gray-200 appearance-none border-3 border-purple-900 rounded w-full px-4 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        placeholder="Your message here..."
+                    />
                 </div>
             </div>
-            <div class="flex flex-row">
-            
-                <div class="basis-1/4 py-5 bg-violet-200 border-2 rounded-md m-5">01</div>
-                <div class="basis-3/4 py-5 bg-violet-200 border-2 rounded-md m-5">03</div>
-            </div>
-            <Link to="/Contact" className="text-sm font-semibold leading-6 text-gray-900">Contact</Link>
+            <form class="w-full max-w-sm">
+                <div class="flex flex-row mb-6">
+                    <div class="basis-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Full Name
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="basis-1/3">
+                        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+                            Email
+                        </label>
+                    </div>
+                    <div class="md:w-2/3">
+                        <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" />
+                    </div>
+                </div>
+                <div class="md:flex md:items-center mb-6">
+                    <div class="md:w-1/3"></div>
+                    <label class="md:w-2/3 block text-gray-500 font-bold">
+                        <input class="mr-2 leading-tight" type="checkbox" />
+                        <span class="text-sm">Send me your newsletter!</span>
+                    </label>
+                </div>
+                <div class="md:flex md:items-center">
+                    <div class="md:w-1/3"></div>
+                    <div class="md:w-2/3">
+                        <button
+                            class="mb-8 shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                            type="button"
+                        >
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </form>
         </PageLayout>
     )
 }
