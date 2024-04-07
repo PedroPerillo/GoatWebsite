39fee8498ce616eef7711e0e1f5b3959fcdbfcd9
Pedro Perillo <pedrofperillo@gmail.com>
2024-04-06 20:45:05 -0600

    changes

--- gymbro/my-project/src/pages/Home.jsx
+++ gymbro/my-project/src/pages/Home.jsx
@@ -344,52 +344,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="mx-auto max-w-7xl sm:px-6 py-36 pb-44 pt-20 lg:px-8">
-          <div className="relative isolate overflow-hidden bg-gray-900 py-24 shadow-2xl sm:rounded-3xl">
-            <h2 className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Fit Check
-            </h2>
-            <p className="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Empower Your Fitness Journey!
-            </p>
-            <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-              Unleash Your Inner Strength
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white hover:text-gray-300"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-            <svg
-              viewBox="0 0 1024 1024"
-              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
-              aria-hidden="true"
-            >
-              <circle
-                cx="512"
-                cy="512"
-                r="512"
-                fill="url(#827591b1-ce8c-4110-b064-7cb85a0b1217)"
-                fill-opacity="0.7"
-              />
-              <defs>
-                <radialGradient id="827591b1-ce8c-4110-b064-7cb85a0b1217">
-                  <stop stop-color="#7775D6" />
-                  <stop offset="1" stop-color="#E935C1" />
-                </radialGradient>
-              </defs>
-            </svg>
-          </div>
-        </div>
+
       </PageLayout>
     </div>
   );
